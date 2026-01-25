5fcd71eb2d59ef24a225fd58a088255a090db4ed
Chirag Mittal <chiragmittal@Chirags-MacBook-Air.local>
2026-01-25 14:07:20 +0530

    feat: refactor db schema

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -9,4 +9,7 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new pg.Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+    adapter,
+    log: ['query', 'info', 'warn', 'error']
+});
